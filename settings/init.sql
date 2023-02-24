105b6cdffca9bb95c281a4868a8c8b4c3c670e11
charlso96 <67186785+charlso96@users.noreply.github.com>
2023-02-24 17:41:28 -0500

    Update init.sql

--- settings/init.sql
+++ settings/init.sql
@@ -10,8 +10,8 @@ set mapred.reduce.parallel.copies=30;
 -- set mapred.job.reduce.input.buffer.percent=0.2;
 set mapred.map.child.java.opts=-server -Xmx2800m -Djava.net.preferIPv4Stack=true;
 set mapred.reduce.child.java.opts=-server -Xmx3800m -Djava.net.preferIPv4Stack=true;
-set mapreduce.map.memory.mb=3072;
-set mapreduce.reduce.memory.mb=4096;
+set mapreduce.map.memory.mb=8192;
+set mapreduce.reduce.memory.mb=8192;
 set hive.llap.memory.oversubscription.max.executors.per.query=8;
 set hive.llap.mapjoin.memory.oversubscribe.factor=0.3;
 set hive.auto.convert.join.hashtable.max.entries=-1;
@@ -21,4 +21,4 @@ set hive.auto.convert.join.shuffle.max.size=10000000000;
 set hive.tez.llap.min.reducer.per.executor=0.33;
 set hive.map.aggr.hash.min.reduction=0.99;
 
-set hive.optimize.sort.dynamic.partition.threshold=0;
+set hive.optimize.sort.dynamic.partition=true;
