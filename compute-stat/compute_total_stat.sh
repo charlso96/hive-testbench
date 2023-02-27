25736febdcd15ee323dc803aaa9b534e9c8545ce
Keonwoo <charlso96@gmail.com>
2023-02-27 03:18:08 -0500

    more experiment

--- compute-stat/compute_total_stat.sh
+++ compute-stat/compute_total_stat.sh
@@ -6,3 +6,6 @@ python3 compute_stat.py -i ./data/listfiles.txt >> ./total_stat.txt
 
 printf "\nsql\n" >> ./total_stat.txt
 python3 compute_stat.py -i ./data/sql.txt >> ./total_stat.txt
+
+printf "\nsql2\n" >> ./total_stat.txt
+python3 compute_stat.py -i ./data/sql2.txt >> ./total_stat.txt
